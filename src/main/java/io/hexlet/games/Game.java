4d48a99470d13a6f2a1cee1b601af097a8ba26a2
ostsle2 <wharfedaleeg@gmail.com>
2021-06-18 18:58:20 +0300

    checkstyle fixes

--- src/main/java/io/hexlet/games/Game.java
+++ src/main/java/io/hexlet/games/Game.java
@@ -2,21 +2,26 @@ package io.hexlet.games;
 
 /**
  * Class Game created as interface to use in different games.
- * */
+ */
 public interface Game {
     /**
      * method getRule() created get game rules.
+     *
      * @return rule
-     * */
+     */
     String getRule();
+
     /**
      * method getQuestion() create to get game question.
+     *
      * @return question
-     * */
+     */
     String getQuestion();
+
     /**
      * method getResult() created to get right result.
+     *
      * @return result
-     * */
+     */
     String getResult();
 }
