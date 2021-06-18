9e36f04bb41c5811d931ff7db1f7eb43ab3fe700
ostsle2 <wharfedaleeg@gmail.com>
2021-06-18 18:35:29 +0300

    add javadock for Game.java

--- src/main/java/io/hexlet/games/Game.java
+++ src/main/java/io/hexlet/games/Game.java
@@ -1,10 +1,24 @@
 package io.hexlet.games;
 
+/**
+ * Class Game created as interface
+ * to use in different games
+ * */
+
 public interface Game {
-
+    /**
+     * method getRule() created
+     * to get game rules
+     * */
     String getRule();
-
+    /**
+     * method getQuestion() created
+     * to get game question
+     * */
     String getQuestion();
-
+    /**
+     * method getResult() created
+     * to get right result
+     * */
     String getResult();
 }
